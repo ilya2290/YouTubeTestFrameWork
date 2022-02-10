d64e36bf8ad09bf3e3f3de71dc9c52f45af66a23
ilya2290 <vasyav371@gmail.com>
2022-02-10 15:55:52 +0200

    Updated All methods Create new single Tests Edited core methods

---
+++ src/main/java/Pages/Page_Core/Page_Core.java
@@ -0,0 +1,43 @@
+package Pages.Page_Core;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Page_Core {
+
+    protected WebDriver driver;
+
+
+    public Page_Core(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void openPage(String URL) {
+        driver.get(URL);
+    }
+
+    public WebElement waitVisibilityOfElement(WebElement element) {  //Наверно сделать метод, которые принимает тайм юнит, чтобы сделать произвольное ожидание по элт
+        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(element));// - посмотреть по локатору, чтобы проверить предварительно наличие в доме presenceOfElementLocated()
+        return element; //Подумать нужен ли войд
+    }
+
+    public By waitAppearanceInDOM(By locator){
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(locator));  //Подумать нужен ли войд
+        return locator;
+    }
+
+    /**
+     * @param element;
+     * @param locator;
+     * This method is using to wait element first in DOM, then wait elements presence in WEB;
+     * @return WebElement;
+     */
+    public WebElement waitDOMAppearanceAndVisibility(WebElement element, By locator) {
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(locator));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOf(element));
+        return element;
+    }
+}
---
+++ src/test/java/Tests/TEST_OpenMainPage/OpenMainPageTest.java
@@ -0,0 +1,20 @@
+package Tests.TEST_OpenMainPage;
+
+
+import Test_Core.Test_Core;
+import org.testng.annotations.Test;
+
+public class OpenMainPageTest extends Test_Core {
+//Приоритет работает в том случае, если несколько тестов в 1 классе. Как задать приоритет для тестов из разных классов - вопрос.
+    @Test(priority = 1)
+    public void checkMainLogoTest() {
+        page_core.openPage("https://www.youtube.com/");
+        mainPage.checkMainLogo();
+    }
+
+    @Test(priority = 2)
+    public void CheckVirtualKeyboardAppearance() {
+        page_core.openPage("https://www.youtube.com/");
+        mainPage.checkVirtualKeyboardPresence();
+    }
+}
